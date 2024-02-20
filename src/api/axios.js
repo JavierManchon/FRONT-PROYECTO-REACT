f38ab4c4a67339006c611d6714fd6df20c9f6e52
JavierManchon <javiermanchongarcia@gmail.com>
2024-02-20 18:02:21 +0100

    Adjustments for vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://back-proyecto-react-eta.vercel.app//api',
     withCredentials: true,
     /*headers: {
         si lo pongo aqui se aplica a todo, tengo que ponerlo solo en los forms que reuqieran archivos
